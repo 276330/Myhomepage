7ba32c8cd7c8ab92331bf94581fd46b1d36a3fba
276330 <276330@glenbrook225.org>
2024-11-07 09:31:04 -0600

    update

--- myScripts.js
+++ myScripts.js
@@ -14,5 +14,30 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
    }
+
+   //POST-IT NOTE SAVE (JAVASCRIPT)
+if (localStorage["note"]) {
+   var note = localStorage["note"];
+   document.getElementById("note").value = note;
+}
+ 
+function autoSave() {
+   var note = document.getElementById("note").value;
+   localStorage.setItem("note", note);
+}
+
+//POST-IT NOTE SHOW AND HIDE
+var show = false;
+function showNote(){
+   if (show == false){
+       document.getElementById("note").style.opacity = 1;
+       show = true;
+   }
+   else if (show == true){
+       document.getElementById("note").style.opacity = 0;
+       show = false;
+   }
+}
+
    
    
